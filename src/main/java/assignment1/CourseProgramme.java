a4d14f40008420b4d7350f328c82d37b6347ba85
Edgars <Edgars@192.168.1.13>
2018-10-30 17:01:40 +0000

    Created classes and initial variables

---
+++ src/main/java/assignment1/CourseProgramme.java
@@ -0,0 +1,15 @@
+package assignment1;
+import java.util.*;
+import org.joda.time.DateTime;
+
+
+public class CourseProgramme {
+    String CourseName;
+    ArrayList listOfModules = new ArrayList();
+    ArrayList enrolledStudents = new ArrayList();
+    DateTime academicStartDate = new DateTime();
+    DateTime academicEndDate = new DateTime();
+    
+
+    
+}
